701a93830c6d52cd16c5a772c9726c68c8af3c9e
Jampanna <dappujampanna@gmail.com>
2024-11-11 00:31:07 +0530

    code added

--- basic.java
+++
@@ -1,6 +0,0 @@
-class basic{
-public static void main(String args[])
-{
-System.out.println("start");
-}
-}
---
+++ demo1.java
@@ -0,0 +1,6 @@
+class demo{
+public static void main(String args[])
+{
+System.out.println("Jampanna-517\nHema sunder-515\nSrilekha-L-8\nAnveshitha-L-2\n");
+}
+}
